5f01edc7dccb034fe3217bbfa96a5bf8b3fbe779
Rikard Eide <rix1@otovo.com>
2018-08-28 20:14:57 +0200

    Extend jsx-a11y/recommended

--- react/index.js
+++ react/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['plugin:react/recommended', 'prettier/react'],
+  extends: [
+    'plugin:react/recommended',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended'
+  ],
   plugins: ['react', 'prettier'],
   rules: {
     'react/jsx-filename-extension': ['off', { extensions: ['.jsx'] }],
