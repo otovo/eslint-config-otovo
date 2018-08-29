1edc7082e096cf108b0f10b7ccf29b47b553010c
Rikard Eide <rix1@otovo.com>
2018-08-29 11:46:20 +0200

    React: Added explaining comments

--- react/index.js
+++ react/index.js
@@ -5,6 +5,17 @@ module.exports = {
   //   "react", added both with Airbnb config and plugin:react/recommended
   // ],
   rules: {
+    // Because Flow can infer default props based on which props are listed in `defaultProps` we disable forbudDefaultForRequired
+    'react/require-default-props': [
+      2,
+      {
+        forbidDefaultForRequired: 0
+      }
+    ],
+    // We want to encourage destructuring, but in order to do this incrementally we'll onyl warng about it.
+    // See https://github.com/yannickcr/eslint-plugin-react/issues/1731 for more info.
+    'react/destructuring-assignment': ['warn', 'always'],
+
     'react/jsx-filename-extension': ['off', { extensions: ['.jsx'] }],
     'react/sort-comp': [
       1,
