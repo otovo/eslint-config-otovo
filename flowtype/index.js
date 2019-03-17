5f6a1ac2764c74d093d00b5a5447341c3c5b0794
Rikard Eide <rix1@otovo.com>
2019-03-17 22:16:29 +0100

    flow: remove comment

--- flowtype/index.js
+++ flowtype/index.js
@@ -1,9 +1,6 @@
 module.exports = {
   extends: ['plugin:flowtype/recommended'],
   parser: 'babel-eslint',
-  // "plugins": [
-  //   "flowtype", added by plugin:flowtype/recommended
-  // ],
   rules: {
     'react/forbid-prop-types': ['warn']
   }
